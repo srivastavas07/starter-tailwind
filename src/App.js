ebbd4fbe341de1e8b22795f0235ac69eae386a1c
Kunal Chandra <72646476+srivastavas07@users.noreply.github.com>
2024-01-15 00:13:16 +0530

    Add files via upload
    Happy Hacking !!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=' underline'>Hello World ! this is tailwind Starter..</h1>
+    </div>
+  );
+}
+
+export default App;
